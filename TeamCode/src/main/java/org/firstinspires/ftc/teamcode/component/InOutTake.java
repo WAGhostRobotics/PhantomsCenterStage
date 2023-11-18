95703a7348a87b8ca2157347ac61f44f858240db
LordVanra <arnav.omkar@gmail.com>
2023-11-18 09:53:33 -0500

    Moved TeleOp stuff to TeleOpParent

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/component/InOutTake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/component/InOutTake.java
@@ -16,8 +16,8 @@ public class InOutTake {
     private double CLOSE;
 
     public void init(HardwareMap hwMap){
-        OPEN=1;
-        CLOSE=1;//Fix this
+        OPEN=0.15;
+        CLOSE=-0.05;
         //replace flap with box1 in competition
         intake = hwMap.get(DcMotor.class, "intake");
         flap = hwMap.get(Servo.class, "flap");
